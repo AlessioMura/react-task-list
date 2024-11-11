b5c128ad4e702ea16d118a8fec58b5726e807f9d
AlessioMura <alessiomura03@gmail.com>
2024-11-11 17:11:52 +0100

    added style

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,6 @@
 export default function AppFooter() {
     return (
-        <footer>Footer</footer>
+        <>
+        </>
     )
 }
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,7 @@
 export default function AppHeader() {
     return (
         <header style={{ backgroundColor: '#7fffd3' }}>
-            <h1 style={{ padding: 30 }} > Task Manager</h1>
+            <h1 style={{ padding: '1rem' }} > Task Manager</h1>
         </header >
     )
 }
